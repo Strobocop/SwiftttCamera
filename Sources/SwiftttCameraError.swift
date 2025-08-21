43bcc755c423d25adb8cb6a0f741d1968bf267c6
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2025-08-21 13:29:46 -0500

    Deleting SwiftttCameraError

--- Sources/SwiftttCameraError.swift
+++
@@ -1,9 +0,0 @@
-// Copyright © 2021 Roger Oba. All rights reserved.
-
-import Foundation
-
-/// An error type that describes possible errors that can occur in the SwiftttCamera.
-public enum SwiftttCameraError: Error, Equatable {
-    /// The camera session timed out while waiting for the view to appear.
-    case cameraSessionTimedOutWaitingForViewToAppear
-}
